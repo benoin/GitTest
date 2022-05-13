63e56660ccf7b40844aaebede8ec59ea7ff88b25
benoin <85065919+benoin@users.noreply.github.com>
2022-05-13 20:27:48 +0800

    Delete src/helloGit directory

--- src/helloGit/helloGit111.java
+++
@@ -1,8 +0,0 @@
-package helloGit;
-
-public class helloGit111 {
-    public static void main(String[] args) {
-        System.out.println("新增功能3.0");
-        System.out.println("新增功能3.1");
-    }
-}
