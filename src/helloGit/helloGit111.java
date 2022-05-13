620c5581cd7c4df9d3fbfc52de47f213a3e44dfa
ZY <ZY@123.com>
2022-05-13 20:11:17 +0800

    功能3.0

--- src/helloGit/helloGit111.java
+++ src/helloGit/helloGit111.java
@@ -2,9 +2,6 @@ package helloGit;
 
 public class helloGit111 {
     public static void main(String[] args) {
-        System.out.println("版本1.0过时");
-        System.out.println("版本2.0");
-        System.out.println("版本2.1");
-        System.out.println("修改版本2.1.1");
+        System.out.println("新增功能3.0");
     }
 }
