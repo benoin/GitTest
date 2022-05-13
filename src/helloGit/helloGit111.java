68b1c8c994930c89acaf99a2a4a1b8a3c3f5c9ae
ZY <ZY@123.com>
2022-05-13 19:55:47 +0800

    NO2.0

--- src/helloGit/helloGit111.java
+++ src/helloGit/helloGit111.java
@@ -2,6 +2,7 @@ package helloGit;
 
 public class helloGit111 {
     public static void main(String[] args) {
-        System.out.println("版本1");
+        System.out.println("版本1.0过时");
+        System.out.println("版本2.0");
     }
 }
